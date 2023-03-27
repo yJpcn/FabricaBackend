41aadd4767453b634e347ec3a71953cac9b56b0f
yJpcn <124182110+yJpcn@users.noreply.github.com>
2023-03-27 17:59:39 -0300

    Update and rename 03.py to lado.py

--- lado.py
+++ lado.py
@@ -1,5 +1,5 @@
 ##3
 lado = int(input("Qual o lado do quadrado?"))
-def multiplo(lado):
+def quadrado(lado):
     print("Esse quadrado tem área igual à",lado**2,".")
-multiplo(lado)    
+quadrado(lado)    
